f3e49306ffc5298ca311a71a28275a8f8fe0586e
benayac <coolcool.rio75@gmail.com>
2022-04-30 16:40:56 +0700

    fix cor 2

--- pkg/middleware/common.go
+++ pkg/middleware/common.go
@@ -4,12 +4,10 @@ import "net/http"
 
 func DefaultHeader(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == "OPTIONS" {
-			w.Header().Set("Access-Control-Allow-Origin", "*")
-			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-			w.Header().Add("Content-Type", "application/json")
-		}
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Add("Content-Type", "application/json")
 		next.ServeHTTP(w, r)
 	})
 
